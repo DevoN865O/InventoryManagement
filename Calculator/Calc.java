3517d5b9ffe7f0beaf48a64f7fe56786c86a6244
devon865o <dnelson924@millardps.org>
2016-12-01 09:28:43 -0600

    created calc

---
+++ Calculator/Calc.java
@@ -0,0 +1,14 @@
+package org.millardps.inventoryManagement.calculator.Runner;
+import java.util.Scanner;
+public static class Calc{
+	Scanner scanner= new Scanner(System.in);
+	private String x= scanner.nextLine();
+	private String num1;
+	private String opr;
+	private String num2;
+	for(int i=0; i<len(x);i++){
+		if(i==" "){
+			
+		}
+	}
+}
