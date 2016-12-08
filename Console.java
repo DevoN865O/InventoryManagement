da138c437fa0b4484fd182c2837119ac190c4480
devon865o <dnelson924@millardps.org>
2016-12-08 00:05:36 -0600

    Added search function.

--- Console.java
+++ Console.java
@@ -9,11 +9,12 @@ public class Console{
     public String[] shifted= new String[]{"m", "l", "8", "q", "A", "P", "R", "W", "C", "h", "n", "k", "1", "7", "z", "B", "o", "9", "2", "g", "f", "6", "p", "O", "0", "a", "e", "3", "j", "Q", "V", "-", "4", "=", "b", "d", "J", "N", " ", "X", "", "M", "I", "U", "J", "i", "5", "y", "c", "w", "*", "T", "$", "%", "S", "v", "^", "L", "+", "&", "Y", "u", "#", "D", "_", "", "r", "Z", "H", "!", "s", "@", "F", "t", "x", "G", "E"};
     private String command;
     private ArrayList<String>tmp= new ArrayList<String>();
-    private ArrayList<String>name= new ArrayList<String>();
-    private ArrayList<String>price= new ArrayList<String>();
-    private ArrayList<String>category= new ArrayList<String>();
-    private ArrayList<String>description= new ArrayList<String>();
-    private ArrayList<String>quantity= new ArrayList<String>();
+    private ArrayList<String>names= new ArrayList<String>();
+    private ArrayList<String>prices= new ArrayList<String>();
+    private ArrayList<String>categories= new ArrayList<String>();
+    private ArrayList<String>descriptions= new ArrayList<String>();
+    private ArrayList<String>quantities= new ArrayList<String>();
+	private ArrayList<String>filter= new ArrayList<String>();
 	Console(){
         popArray();
         System.out.println("Oxi Inventory Management [Version Alpha 0.1]");
@@ -22,6 +23,7 @@ public class Console{
         nextCommand();
     }
 	public void nextCommand(){
+		updateArrays();
         System.out.print(">");
         command = scanner.nextLine();
         try{
@@ -48,23 +50,44 @@ public class Console{
             } 
 			else if(command.toLowerCase().equals("about")){
                 about();
-            } 
+            }
+			else if(command.toLowerCase().equals("display")){
+				for(int i=0;i<tmp.size();i++){
+					System.out.println(tmp.get(i));
+				}
+				nextCommand();
+			}
 			else if(command.toLowerCase().equals("delete")){
 				m.delete();
 				tmp.clear();
 				System.out.println("Inventory Cleared!");
 				nextCommand();
 			}
-			else if(command.toLowerCase().equals("display")){
-                search();
+			else if(command.toLowerCase().substring(0,6).equals("search")){
+                if(command.length()>6){
+					search(command);
+				}
+				else{
+					System.out.println("Searches the inventory based upon given criteria.");
+                    System.out.println();
+                    System.out.println("USAGE:");
+                    System.out.println("search [/N] [/C] [/P] [/D]");
+                    System.out.println();
+                    System.out.println("  /N           Item name.");
+                    System.out.println("  /C           Item category.");
+                    System.out.println("  /P           Item price.");
+                    System.out.println("  /D           Item description.");
+                    System.out.println("  /Q           Item quantity.");
+                    nextCommand();
+				}
             } 
 			else if(command.toLowerCase().equals("sell")){
                 sell();
             } 
-			else if(command.toLowerCase().equals("clear")){
+			else if(command.toLowerCase().equals("clear")){//claims command does not exist
                 clear();
             } 
-			else if(command.toLowerCase().equals("exit")){
+			else if(command.toLowerCase().equals("exit")){//claims command does not exist
                 writeArray();
             } 
 			else if(command.toLowerCase().substring(0, 3).equals("cat")){
@@ -177,20 +200,109 @@ public class Console{
             while(description.substring(0,i).contains("/")){
                 i--;
             }
-            addString+=description.substring(0,i).trim()+"-";
+            addString+=description.substring(0,i).trim();
         }
 		else{
-            addString+="null-";
+            addString+="null";
         }
         tmp.add(addString);
         System.out.println("Item added to inventory: "+addString+".");
         nextCommand();
     }
-    public void search(){
-        for(int i = 0; i < tmp.size(); ++i){
-            System.out.println(tmp.get(i));
+    public void search(String command){
+		String name="default0x0";
+		String price="default0x0";
+		String category="default0x0";
+		String description="default0x0";
+		String quantity="default0x0";
+        if(command.contains("/n")){
+            int i=0;
+            while(command.substring(i).contains("/n")){
+                i++;
+            }
+            name=command.substring(i+1);
+            i=name.length();
+            while(name.substring(0,i).contains("/")){
+                i--;
+            }
+            name=name.substring(0,i).trim();
         }
-        nextCommand();
+		if(command.contains("/q")){
+            int i=0;
+            while(command.substring(i).contains("/q")){
+                i++;
+            }
+            quantity=command.substring(i+1);
+            i=quantity.length();
+            while(quantity.substring(0,i).contains("/")){
+                i--;
+            }
+            quantity=quantity.substring(0,i).trim();
+        }
+		if(command.contains("/c")){
+            int i=0;
+            while(command.substring(i).contains("/c")){
+                i++;
+            }
+            category=command.substring(i+1);
+            i=category.length();
+            while(category.substring(0,i).contains("/")){
+                i--;
+            }
+            category=category.substring(0,i).trim();
+        }
+		if(command.contains("/p")){
+            int i=0;
+            while(command.substring(i).contains("/p")){
+                i++;
+            }
+            name=command.substring(i+1);
+            i=price.length();
+            while(name.substring(0,i).contains("/")){
+                i--;
+            }
+            price=price.substring(0,i).trim();
+        }
+		if(command.contains("/d")){
+            int i=0;
+            while(command.substring(i).contains("/d")){
+                i++;
+            }
+            description=command.substring(i+1);
+            i=description.length();
+            while(description.substring(0,i).contains("/")){
+                i--;
+            }
+            description=description.substring(0,i).trim();
+        }
+		filter.clear();
+		for(int i=0;i<tmp.size();i++){
+			filter.add(tmp.get(i));
+		}
+		for(int i=0;i<filter.size();i++){
+			if((! name.equals("default0x0") && (! names.get(i).equals(name)))){
+			filter.set(i, "!!!!remove!!!!");
+			}
+			if((! quantity.equals("default0x0") && (! quantities.get(i).equals(name)))){
+				filter.set(i, "!!!!remove!!!!");
+			}
+			if((! category.equals("default0x0") && (! categories.get(i).equals(name)))){
+				filter.set(i, "!!!!remove!!!!");
+			}
+			if((! price.equals("default0x0") && (! prices.get(i).equals(name)))){
+				filter.set(i, "!!!!remove!!!!");
+			}
+			if((! description.equals("default0x0") && (! descriptions.get(i).equals(name)))){
+				filter.set(i, "!!!!remove!!!!");
+			}
+			if(filter.get(i).equals("!!!!remove!!!!")){
+				filter.remove(i);
+			}
+		}
+		for(int i=0;i<filter.size();i++){
+			System.out.println(filter.get(i));
+		}
+		nextCommand();
     }
     public void about(){
         try{
@@ -242,6 +354,36 @@ public class Console{
             System.out.println("FileNotFoundException");
         }
     }
+	public void updateArrays(){
+		names.clear();
+		quantities.clear();
+		prices.clear();
+		descriptions.clear();
+		categories.clear();
+		for(int i=0;i<tmp.size();i++){
+			int x=0;
+			while(! tmp.get(i).substring(0,x).contains("-")){
+				x++;
+			}
+			names.add(tmp.get(i).substring(0,x-2).trim());
+			int x1=x;
+			while(! tmp.get(i).substring(x,x1).contains("-")){
+				x1++;
+			}
+			quantities.add(tmp.get(i).substring(x,x1-1).trim());
+			int x2=x1;
+			while(! tmp.get(i).substring(x1,x2).contains("-")){
+				x2++;
+			}
+			categories.add(tmp.get(i).substring(x1,x2-1).trim());
+			int x3=x2;
+			while(! tmp.get(i).substring(x2,x3).contains("-")){
+				x3++;
+			}
+			prices.add(tmp.get(i).substring(x2,x3-1).trim());
+			descriptions.add(tmp.get(i).substring(x3,tmp.get(i).length()-1).trim());
+		}
+	}
 	public void writeArray(){
         m.delete();
         try{
